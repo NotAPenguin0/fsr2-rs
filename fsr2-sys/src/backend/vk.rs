e1ab9a5138eb9113ba421b68758e3a3ab28d31ce
NotAPenguin <michiel.vandeginste@gmail.com>
2023-05-24 11:47:51 +0200

    change: pass instance function fp table to fsr2 library to avoid linking to vulkan at all

--- fsr2-sys/src/backend/vk.rs
+++ fsr2-sys/src/backend/vk.rs
@@ -17,6 +17,16 @@ impl VkPhysicalDevice {
 
 #[derive(Copy, Clone)]
 #[repr(transparent)]
+pub struct VkInstance(u64);
+
+impl VkInstance {
+    pub fn from_raw(value: u64) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(transparent)]
 pub struct VkDevice(u64);
 
 impl VkDevice {
@@ -67,19 +77,81 @@ impl VkBuffer {
 
 pub type VkFormat = i32;
 pub type VkImageLayout = i32;
+pub type VkFlags = u32;
+pub type VkMemoryPropertyFlags = VkFlags;
+pub type VkMemoryHeapFlags = VkFlags;
+pub type VkDeviceSize = u64;
+pub type VkPhysicalDeviceType = i32;
+
+pub const VK_MAX_EXTENSION_NAME_SIZE: usize = 256;
+pub const VK_MAX_MEMORY_TYPES: usize = 32;
+pub const VK_MAX_MEMORY_HEAPS: usize = 32;
+pub const VK_MAX_PHYSICAL_DEVICE_NAME_SIZE: usize = 256;
+pub const VK_UUID_SIZE: usize = 16;
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct VkExtensionProperties {
+    pub extension_name: [c_char; VK_MAX_EXTENSION_NAME_SIZE],
+    pub spec_version: u32,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct VkMemoryType {
+    pub property_flags: VkMemoryPropertyFlags,
+    pub heap_index: u32,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct VkMemoryHeap {
+    pub size: VkDeviceSize,
+    pub flags: VkMemoryHeapFlags,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct VkPhysicalDeviceMemoryProperties {
+    pub memory_type_count: u32,
+    pub memory_types: [VkMemoryType; VK_MAX_MEMORY_TYPES],
+    pub memory_heap_count: u32,
+    pub memory_heaps: [VkMemoryHeap; VK_MAX_MEMORY_HEAPS],
+}
 
 pub type VoidFunctionPtr = Option<unsafe extern "system" fn()>;
-pub type VkGetDeviceProcAddrFunc = unsafe extern "system" fn(VkDevice, *const c_char) -> VoidFunctionPtr;
+
+pub type VkEnumerateDeviceExtensionPropertiesFunc = unsafe extern "system" fn(VkPhysicalDevice, *const c_char, *mut u32, *mut VkExtensionProperties);
+
+pub type VkGetDeviceProcAddrFunc =
+    unsafe extern "system" fn(VkDevice, *const c_char) -> VoidFunctionPtr;
+
+pub type VkGetPhysicalDeviceMemoryPropertiesFunc = unsafe extern "system" fn(VkPhysicalDevice, *mut VkPhysicalDeviceMemoryProperties);
+
+/// fn(VkPhysicalDevice, *mut VkPhysicalDeviceProperties)
+/// This type is not fully spelled out to avoid depending on ash or another vulkan loader for the vulkan types
+pub type VkGetPhysicalDevicePropertiesFunc = unsafe extern "system" fn(VkPhysicalDevice, *mut c_void);
+
+/// fn(VkPhysicalDevice, *mut VkPhysicalDeviceProperties2)
+/// This type is not fully spelled out to avoid depending on ash or another vulkan loader for the vulkan types
+pub type VkGetPhysicalDeviceProperties2Func = unsafe extern "system" fn(VkPhysicalDevice, *mut c_void);
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct FfxFsr2InstanceFunctionPointerTableVk {
+
+    fp_get_device_proc_addr: VkGetDeviceProcAddrFunc,
+}
 
 extern "C" {
-    pub fn ffxFsr2GetScratchMemorySizeVK(device: VkPhysicalDevice) -> usize;
+    pub fn ffxFsr2GetScratchMemorySizeVK(device: VkPhysicalDevice, fp_table: *const FfxFsr2InstanceFunctionPointerTableVk) -> usize;
 
     pub fn ffxFsr2GetInterfaceVK(
         interface: *mut FfxFsr2Interface,
         scratch_buffer: *mut c_void,
         scratch_size: usize,
         device: VkPhysicalDevice,
-        fp_get_device_proc_addr: VkGetDeviceProcAddrFunc,
+        fp_table: *const FfxFsr2InstanceFunctionPointerTableVk,
     ) -> FfxErrorCode;
 
     pub fn ffxGetDeviceVK(device: VkDevice) -> FfxDevice;
